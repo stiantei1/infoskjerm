41c497018ac67edf571f56c92b973ad30aa2ebc8
Stian Teien <stianteien@Stians-MacBook-Pro.local>
2020-06-29 13:36:29 +0200

    initonal commit

---
+++ src/Bikes.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect } from "react";
+import createEnturService from '@entur/sdk';
+
+
+const service = createEnturService({ clientName: 'stianteien-infoskjerm' })
+
+const Bikes = () => {
+    const [bikeStations, setBikeStations] = useState([]);
+
+    useEffect(() => {
+        service.getBikeRentalStationsByPosition(
+            {latitude: 59.9289553, longitude: 10.7586829},
+            230
+        ).then((data) => setBikeStations(data));
+    }, []);
+
+    console.log(bikeStations);
+    return ( <div className="BikeStations">
+        <h3>Bysykkel</h3>
+        {bikeStations.map((stationData) => (
+            <Station key={stationData.id} station={stationData} />
+        ))}
+        </div>
+    );
+};
+
+export default Bikes;
+
+
+const Station = (props) => {
+    const { station } = props;
+    console.log(station);
+
+    return (
+    <div className="station">
+        {station.name} - {station.bikesAvailable} : {station.spacesAvailable}
+
+    </div>
+    );
+};
---
+++ src/Reddit.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const APIurl = "https://www.reddit.com/r/dadjokes/top.json?t=day&limit=1";
+
+const Reddit = () => {
+    const [joke, setJoke] = useState({});
+    const getJoke = () => {
+        fetch(APIurl)
+        .then((response) => response.json())
+        .then((json) => setJoke(json.data.children[0].data))
+        .catch((err) => console.error(err));
+    };
+
+    useEffect(() => {
+        getJoke();
+    }, []);
+
+    return joke ? (<div className="reddit">
+        <p>{joke.title}</p>
+        <p>{joke.selftext}</p>
+    </div>
+    ) : (
+        <></>
+    );
+};
+
+export default Reddit;
