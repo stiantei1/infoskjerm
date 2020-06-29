ffb27b0576a95c94a91485bca45158de80ace96a
Stian Teien <stianteien@Stians-MacBook-Pro.local>
2020-06-29 13:42:46 +0200

    Updates

---
+++ precache-manifest.87b3cfb34fc77baa0a93c3c8b40a4818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b43cded3120261bf41c8ad7a50d9be",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "5e2380a2e69e4296aa71",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "54e0c7c0ef49d6f7accb",
+    "url": "/infoskjerm/static/js/2.af637332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.af637332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2380a2e69e4296aa71",
+    "url": "/infoskjerm/static/js/main.9d4e7f6f.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
